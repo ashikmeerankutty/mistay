d02ab5b9683798bfbdae784e8d399f16555433c2
Ashik Meerankutty <ashik9591@gmail.com>
2020-01-14 23:18:07 +0530

    Datepicker: Added today to datelist

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -1,6 +1,7 @@
 const generateDateTimeList = (startDate, count) => {
   let start = startDate
   const dateTimeList = []
+  dateTimeList.push(start)
   for (let i = 0; i < count; i += 1) {
     const nextDay = new Date(start)
     nextDay.setDate(nextDay.getDate() + 1)
