10d68388fed156e23cc50fbb7e3479b768b171af
Ashik Meerankutty <ashik9591@gmail.com>
2020-01-14 23:02:13 +0530

    Datepicker: Implemented function to generate dates

---
+++ src/utils/dates.js
@@ -0,0 +1,14 @@
+const generateDateTimeList = (startDate, count) => {
+  let start = startDate
+  const dateTimeList = []
+  for (let i = 0; i < count; i += 1) {
+    const nextDay = new Date(start)
+    nextDay.setDate(nextDay.getDate() + 1)
+    nextDay.setHours(0, 0, 0, 0)
+    start = nextDay
+    dateTimeList.push(nextDay)
+  }
+  return dateTimeList
+}
+
+export default generateDateTimeList
